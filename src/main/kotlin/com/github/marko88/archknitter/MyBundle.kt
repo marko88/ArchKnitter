3a789f840ee2116045e52d42469f35cec65a6e12
GitHub Action <action@github.com>
2025-05-10 16:58:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/marko88/archknitter/MyBundle.kt
+++ src/main/kotlin/com/github/marko88/archknitter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.marko88.archknitter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/marko88/archknitter/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/marko88/archknitter/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.marko88.archknitter.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
